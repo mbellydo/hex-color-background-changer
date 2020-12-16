36fc7ee910eeabff3c365194e495f0b8004ac450
marc <mbellydo@gmail.com>
2020-12-16 12:01:39 +0100

    Exercise done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 // Tu código aquí
+let button = document.querySelector('a')
+
+button.onclick = eventClick
+
+function generarLetra(){
+	let letras = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
+	let numero = (Math.random()*15).toFixed(0);
+	return letras[numero];
+}
+function generarHex(){
+    let colorHex = ""
+	for(let i=0 ; i<6 ; i++){
+	    colorHex = colorHex + generarLetra()
+	}
+	return "#" + colorHex
+}
+
+function eventClick(){
+    let color = generarHex()
+    let body = document.querySelector('body')
+
+    body.style.backgroundColor = color
+}
+
+//<a href="#" id="btn" class="btn btn-secondary text-uppercase my-2">click me</a>
